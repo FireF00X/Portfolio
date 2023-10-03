46d74a150a51a075fd2957b6a5f1853bc3f35bf4
ahmed <j.ahmed.elsaied@gmail.com>
2023-10-03 23:24:08 +0300

    commit-one

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Hero from '../components/portofolio/hero/Hero'
 import './mainLayout.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate } from '@fortawesome/free-solid-svg-icons';
 
 const MainLayout = ({ children }) => {
+    const [rotate, setRotate] = useState(false)
     return (
         <main>
-            <div className='container'>
+            <div className={`container ${rotate ? 'rotate' : ''}`}>
+                <div onClick={_ => setRotate(!rotate)}>
+                    <FontAwesomeIcon icon={faRotate} />
+                </div>
                 <section>
                     <Hero />
                 </section>
